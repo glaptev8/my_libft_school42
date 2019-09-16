85aceb534b04b31a6722396631cba8d66e8a70cf
glaptev8 <gleb.laptev.00@bk.ru>
2019-09-16 13:00:19 +0300

    finished

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmelia <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/16 12:52:07 by tmelia            #+#    #+#             */
+/*   Updated: 2019/09/16 12:52:21 by tmelia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list *tmp;
 
@@ -13,4 +25,3 @@ void ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 	}
 	*alst = NULL;
 }
-
